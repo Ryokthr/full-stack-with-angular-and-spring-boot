e0ff1e99c4a42a4f94b76012645d834e5338c5a7
kitahararyou <kitahararyou@localhost>
2023-12-20 21:46:48 +0900

    ~step24

---
+++ todo/src/app/list-todos/list-todos.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ){}
+}
+
+@Component({
+  selector: 'app-list-todos',
+  templateUrl: './list-todos.component.html',
+  styleUrls: ['./list-todos.component.css']
+})
+export class ListTodosComponent {
+
+  todos = [
+    new Todo(1, 'Learn to Dance', false, new Date()),
+    new Todo(2, 'Become an Expert at Angular', false, new Date()),
+    new Todo(3, 'Visit India', false, new Date()),
+    // { id: 1, description: 'Learn to Dance' },
+    // { id: 2, description: 'Become an Expert at Angular' },
+    // { id: 3, description: 'Visit India' },
+  ]
+
+  // todo = {
+  //   id: 1,
+  //   description: 'Learn to Dance'
+  // }
+}
