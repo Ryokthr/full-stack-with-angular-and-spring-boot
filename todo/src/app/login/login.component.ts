ab9a42eef8869c7de0b9c0a7fc543f8af59974b7
kitahararyou <kitahararyou@localhost>
2023-12-23 12:34:31 +0900

    section3 finished

--- todo/src/app/login/login.component.ts
+++ todo/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +13,13 @@ export class LoginComponent {
   errorMessage = 'Invalid Credentials';
   invalidLogin = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   handleLogin() {
     // console.log(this.username);
-    if (this.username === "in28minutes" && this.password === 'dummy') {
+    // if (this.username === "in28minutes" && this.password === 'dummy') {
+    if (this.hardcodedAuthenticationService.authenticate(this.username, this.password)) {
       this.router.navigate(['welcome', this.username]);
       this.invalidLogin = false;
     } else {
