c545b9f867e00730fab239390c1532a20cf6ac70
kitahararyou <kitahararyou@localhost>
2024-01-16 22:54:03 +0900

    Todo削除機能追加

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -13,12 +13,32 @@ public class TodoHardcodedService {
 	private static int idCounter = 0;
 
 	static {
-		todos.add(new Todo(++idCounter, "in28Minutes", "Learn to Dance", new Date(), false));
-		todos.add(new Todo(++idCounter, "in28Minutes", "Learn about Microservices", new Date(), false));
+		todos.add(new Todo(++idCounter, "in28Minutes", "Learn to Dance 2", new Date(), false));
+		todos.add(new Todo(++idCounter, "in28Minutes", "Learn about Microservices 2", new Date(), false));
 		todos.add(new Todo(++idCounter, "in28Minutes", "Learn about Angular", new Date(), false));
 	}
 
 	public List<Todo> findAll() {
 		return todos;
+	}
+	
+	public Todo deleteById(long id) {
+		Todo todo = findById(id);
+		if (todo == null) {
+			return null;
+		}
+		if (todos.remove(todo)) {
+			return todo;
+		}
+		return null;
+	}
+
+	public Todo findById(long id) {
+		for (Todo todo : todos) {
+			if (todo.getId() == id) {
+				return todo;
+			}
+		}
+		return null;
 	}
 }
--- todo/src/app/list-todos/list-todos.component.ts
+++ todo/src/app/list-todos/list-todos.component.ts
@@ -18,6 +18,8 @@ export class Todo {
 export class ListTodosComponent implements OnInit {
 
   todos: Todo[] = [];
+
+  message: string = '';
   // todos = [
   //   new Todo(1, 'Learn to Dance', false, new Date()),
   //   new Todo(2, 'Become an Expert at Angular', false, new Date()),
@@ -37,14 +39,29 @@ export class ListTodosComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-      this.todoService.retrieveAllTodos('in28minutes').subscribe(
-        response => {
-          console.log(response);
-          this.todos = response;
-        },
-        error => {
-          console.log(error.error);
-        }
-      );
+      this.refreshTodos();
+  }
+
+  deleteTodo(id: number): void {
+    console.log(`delete todo ${id}`);
+    this.todoService.deleteTodo('in28minutes', id).subscribe(
+      response => {
+        console.log(response);
+        this.message =  `Todo ${id} is successfully deleted`;
+        this.refreshTodos();
+      }
+    )
+  }
+
+  refreshTodos(): void {
+    this.todoService.retrieveAllTodos('in28minutes').subscribe(
+      response => {
+        console.log(response);
+        this.todos = response;
+      },
+      error => {
+        console.log(error.error);
+      }
+    );
   }
 }
