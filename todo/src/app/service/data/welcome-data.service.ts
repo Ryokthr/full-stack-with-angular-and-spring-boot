ef3dcd57a30204147eb4ba2125b5198d21e0eca4
kitahararyou <kitahararyou@localhost>
2024-01-21 00:43:10 +0900

    SpringSecurityConfigurationBasicAuth 追加

--- todo/src/app/service/data/welcome-data.service.ts
+++ todo/src/app/service/data/welcome-data.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export class HelloWorldBean {
-  constructor(public message: string){}
+  constructor(public message: string) { }
 }
 
 @Injectable({
@@ -19,7 +19,21 @@ export class WelcomeDataService {
     // console.log("Execute Hello World Bean Service");
   }
 
-  executeHelloWorldBeanServiceWithPathVariable(name : string) {
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
+  executeHelloWorldBeanServiceWithPathVariable(name: string) {
+    let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    })
+
+    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`, { headers });
+  }
+
+  createBasicAuthenticationHttpHeader() {
+    let username = 'in28minutes';
+    let password = 'dummy'
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    return basicAuthHeaderString;
   }
 }
