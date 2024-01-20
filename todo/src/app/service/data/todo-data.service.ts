39118d3f904b2f64cbcb910e25914d90a8e69add
kitahararyou <kitahararyou@localhost>
2024-01-20 16:58:27 +0900

    フロント 更新処理追加

--- todo/src/app/service/data/todo-data.service.ts
+++ todo/src/app/service/data/todo-data.service.ts
@@ -22,4 +22,8 @@ export class TodoDataService {
   deleteTodo(username: string, id: number) {
     return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
   }
+
+  updateTodo(username: string, id: number, todo: Todo) {
+    return this.http.put(`http://localhost:8080/users/${username}/todos/${id}`, todo);
+  }
 }
--- todo/src/app/todo/todo.component.ts
+++ todo/src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TodoDataService } from '../service/data/todo-data.service';
 import { Todo } from '../list-todos/list-todos.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -15,19 +15,23 @@ export class TodoComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private todoService: TodoDataService
+    private todoService: TodoDataService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
     this.todo = new Todo(1, '', false, new Date())
     this.todoService.retrieveTodo('in28minutes', this.id)
-        .subscribe(
-          data => this.todo = data
-        );
+      .subscribe(
+        data => this.todo = data
+      );
   }
 
   saveTodo(): void {
-
+    this.todoService.updateTodo('in28minutes', this.id, this.todo).subscribe(data => {
+      console.log(data);
+      this.router.navigate(['todos']);
+    })
   }
 }
