43b4014d6304e1ec3fcbbd26f709b6c11bc4e408
kitahararyou <kitahararyou@localhost>
2024-01-21 09:53:57 +0900

    HttpIntercepter 追加

---
+++ todo/src/app/service/http/http-intercepter-basic-auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpIntercepterBasicAuthService implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let username = 'in28minutes';
+    let password = 'dummy'
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    req = req.clone({
+      setHeaders: {
+        Authorization: basicAuthHeaderString
+      }
+    })
+
+    return next.handle(req);
+  }
+}
